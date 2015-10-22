8a4ffab0518c645fed154a3925d8c2667f9cb624
kyle macpherson <kaimac2010@gmail.com>
2015-10-22 07:41:13 +0100

    added unit tests for likes, comments and posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Post, type: :model do
   it { is_expected.to have_many :comments }
   it { is_expected.to have_many :likes }
+  it { is_expected.to belong_to :user}
+
 
   it 'is not valid without user providing content' do
     post = Post.new(content: "")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe User do
 
-  it {is_expected.to have_many :liked_posts}
   it {is_expected.to have_many :posts}
   it {is_expected.to have_many :comments}
+  it {is_expected.to have_many :liked_posts}
 
 end
