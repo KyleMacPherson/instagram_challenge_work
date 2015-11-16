7df0b6401fde933ca81ecd3c8202ee0a820c131d
kyle macpherson <kaimac2010@gmail.com>
2015-11-16 14:11:28 +0000

    styling page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017195233) do
+ActiveRecord::Schema.define(version: 20151116123305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20151017195233) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+    t.string   "email"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
